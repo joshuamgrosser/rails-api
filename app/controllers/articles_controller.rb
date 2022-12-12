eb3fe020b102f97af69de644b193207f3a4afec9
joshuamgrosser <joshua.grosser@gmail.com>
2022-12-12 09:23:03 -0500

    feat: crud for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,39 @@ class ArticlesController < ApplicationController
     @articles = Article.all
     render json: @articles
   end
+
+  def show
+    @article = Article.find(params[:id])
+    render json: @article
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render json: @article
+    else
+      render json: @article.errors
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    render json: { message: "Article #{@article.id} deleted" }
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
